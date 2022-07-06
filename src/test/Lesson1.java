7c30f21693622628f1c10d828da6fffc18284556
Maryna Altsyvanovich <marynaa@everc.com>
2022-07-06 17:25:10 +0300

    add dependencies and new class

---
+++ src/test/Lesson1.java
@@ -0,0 +1,8 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+
+        // System Property for Chrome Driver
+        System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
+
+    }
+}
