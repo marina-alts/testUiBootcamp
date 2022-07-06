42b1a4cc1f25122935793ea7db46f9564ec99f61
Maryna Altsyvanovich <marynaa@everc.com>
2022-07-06 17:39:39 +0300

    add dependencies and new class

--- src/test/Lesson1.java
+++ src/test/Lesson1.java
@@ -1,8 +1,23 @@
 public class Lesson1 {
-    public static void main(String[] args) {
 
-        // System Property for Chrome Driver
-        System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
+        @Test
+        void openPage(){
 
-    }
+                System.setProperty("webdriver.chrome.driver", "chromedriver");
+                //webdriver instance
+                WebDriver driver = new ChromeDriver();
+                // implicit wait
+                driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+                //url launch
+                driver.get("https://www.tutorialspoint.com/index.htm");
+                //element identify
+                WebElement elm = driver.findElement(By.tagName("input"));
+                //perform action - input text
+                elm.sendKeys("Selenium");
+                String s = elm.getAttribute("value");
+                //validate result with Assertion
+                Assert.assertEquals(s, "Selenium");
+                //quit browser
+                driver.quit();
+        }
 }
