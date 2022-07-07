0b08a9c67d8e4dd44ce5f721111668c2749c29d5
Maryna Altsyvanovich <marynaa@everc.com>
2022-07-07 09:51:22 +0300

    add dependencies and new class

--- src/test/java/Lesson1Test.java
+++ src/test/java/Lesson1Test.java
@@ -1,9 +1,5 @@
-import java.util.concurrent.TimeUnit;
-
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Lesson1Test {
