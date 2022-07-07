4dc157bac6903d54c13c5d91b89f48814341665b
Maryna Altsyvanovich <marynaa@everc.com>
2022-07-07 10:03:11 +0300

    use webdrivermanager

--- src/test/java/Lesson1Test.java
+++ src/test/java/Lesson1Test.java
@@ -1,13 +1,16 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Lesson1Test {
 
         @Test
         void openPage(){
 
-                System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+                //System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+
+                WebDriverManager.chromedriver().setup();
 
                 WebDriver driver = new ChromeDriver();
 
