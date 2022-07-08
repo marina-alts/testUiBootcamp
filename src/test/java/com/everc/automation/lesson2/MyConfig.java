b11c96d51c18691cda1aee3189df5b8d14c4cad2
Maryna Altsyvanovich <marynaa@everc.com>
2022-07-08 17:17:02 +0300

    lesson 2 - 4 tests

--- src/test/java/com/everc/automation/lesson2/MyConfig.java
+++ src/test/java/com/everc/automation/lesson2/MyConfig.java
@@ -1,6 +1,6 @@
 package com.everc.automation.lesson2;
 
- @org.aeonbits.owner.Config.Sources({"file:config.properties"})
+ @org.aeonbits.owner.Config.Sources({"file:.src/test/resources/config/config.properties"})
 
 public interface MyConfig extends org.aeonbits.owner.Config {
     String browser();
